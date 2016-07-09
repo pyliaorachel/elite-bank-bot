2598da960ebc75e4ceecf22da7684d7bf0908726
xul4284g <u3512485@connect.hku.hk>
2016-07-09 16:44:14 +0800

    Successful webhook

--- functions/bot/handler.js
+++ functions/bot/handler.js
@@ -1,46 +1,11 @@
 'use strict';
 
-var request = require('request')
-
-function display(object) {
-    return JSON.stringify(object, null, 2)
-}
-
 module.exports.handler = function(event, context) {
-
-    console.log('Event: ', display(event))
-    const operation = event.operation
-    const secret = event.secret
-    const token = event.access_token 
-    
-    function sendTextMessage(sender, text) {
-        const messageData = {text: text}
-        console.log('Ready to send text message', text);
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: {
-                access_token: token
-            },
-            method: 'POST',
-            json: {
-                recipient: {
-                    id: sender
-                },
-                message: messageData
-            }
-        }, (error, response, body) => {
-            console.log('GET response', response);
-            context.succeed(response);
-            if (error) {
-                context.fail('Error sending message: ', error);
-            } else if (response.body.error) {
-                context.fail('Error: ', response.body.error);
-            }
-        })
-    }
-
+  const operation = event.operation
+  console.log("Event: "+ event)
     switch (operation) {
         case 'verify':
+            const secret = event.secret
             const verifyToken = event["verify_token"]
             if (secret === verifyToken) {
                 context.succeed(parseInt(event["challenge"]))
@@ -48,23 +13,7 @@ module.exports.handler = function(event, context) {
                 context.fail(new Error('Unmatch'))
             }
             break
-        case 'reply':
-            const messagingEvents = event.body.entry[0].messaging
-            messagingEvents.forEach((messagingEvent) => {
-                const sender = messagingEvent.sender.id
-                if (messagingEvent.message && messagingEvent.message.text) {
-                    const text = messagingEvent.message.text
-                    console.log('In case reply', text);
-                    sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200))
-                }
-            })
-            break
-        case 'postEvent':
-            console.log(event)
-            //context.succeed()
-            break
         default:
             context.fail(new Error('Unrecognized operation "' + operation + '"'))
     }
-
-}
+};
