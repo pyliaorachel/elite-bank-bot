f4e11fa8ccc5de570b5629bd1b4e816cf294c2c6
xul4284g <u3512485@connect.hku.hk>
2016-07-09 16:57:55 +0800

    Echo message to user

--- functions/postEvent/handler.js
+++ functions/postEvent/handler.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var request = require('request')
-// var aws = require('aws-sdk')
-// aws.config.region = 'ap-northeast-1';
+var aws = require('aws-sdk')
+aws.config.region = 'ap-northeast-1';
 
 function display(object) {
     return JSON.stringify(object, null, 2)
@@ -25,21 +25,21 @@ module.exports.handler = function(event, context) {
             console.log(str)
             var json = JSON.parse(str)
 
-   //          // call bot
-			// var lambda = new aws.Lambda();
-			// var params = {
-			// 	FunctionName: 'bot',
-			// 	InvocationType: 'Event',
-			// 	LogType: 'None',
-			// 	Payload: json,
-			// };
-			// lambda.invoke(params, function(error, data) {
-			// 	if (error) {
-			// 		console.log(error); // error is Response.error
-			// 	} else {
-			// 		console.log(data); // data is Response.data
-			// 	}
-			// });
+            // call bot
+			var lambda = new aws.Lambda();
+			var params = {
+				FunctionName: 'bot',
+				InvocationType: 'Event',
+				LogType: 'None',
+				Payload: json,
+			};
+			lambda.invoke(params, function(error, data) {
+				if (error) {
+					console.log(error); // error is Response.error
+				} else {
+					console.log(data); // data is Response.data
+				}
+			});
 
             context.succeed(json)
             break
