29cedfe59d85d8d4d25f36e9e2ac0556c0a5dbf3
xul4284g <u3512485@connect.hku.hk>
2016-07-11 23:49:21 +0800

    Send market event report url to clients.

--- functions/bot/handler.js
+++ functions/bot/handler.js
@@ -3,7 +3,7 @@
 var request = require('request')
 
 // define hard-coded data
-var phoneNumber = "(00)-0000-0000" // phone number of Elite Butler
+var phoneNumber = "0000-0000" // phone number of Elite Butler
 var investorList = ["1", "2", "3", "4", "5", "6", "7", "1120359171369250"]; // messenger ids of registered clients
 
 function display(object) {
@@ -17,8 +17,9 @@ module.exports.handler = function(event, context) {
     const secret = event.secret
     const token = event.access_token 
     
-    function sendTextMessage(sender, text, token) {
-        const messageData = {text: text}
+    function sendTextMessage(id, text, resolve) {
+
+        const messageData = {text: text};
         console.log('Ready to send text message', text);
         request({
             url: 'https://graph.facebook.com/v2.6/me/messages',
@@ -28,82 +29,227 @@ module.exports.handler = function(event, context) {
             method: 'POST',
             json: {
                 recipient: {
-                    id: sender
+                    id: id
                 },
                 message: messageData
             }
         }, (error, response, body) => {
-            console.log('GET response', response);
-            context.succeed(response);
+            console.log('GET response from ', response.request.body);
             if (error) {
-                context.fail('Error sending message: ', error);
+                console.log('Error sending message: '+ error);
             } else if (response.body.error) {
-                context.fail('Error: ', response.body.error);
+                var json = JSON.parse(response.request.body)
+                console.log('Error: '+ response.body.error);
             }
+            resolve();
         })
     }
+
+    function sendUrlMessage(id, url, title, resolve) {
+        var messageData = {
+            recipient: {
+              id: id
+            },
+            message: {
+              attachment: {
+                type: "template",
+                payload: {
+                  template_type: "generic",
+                  elements: [{
+                    title: title,
+                    subtitle: new Date(),
+                    item_url: url,               
+                    image_url: "https://upload.wikimedia.org/wikipedia/en/thumb/a/ab/UBS_Logo.svg/1280px-UBS_Logo.svg.png",
+                    buttons: [{
+                      type: "web_url",
+                      url: url,
+                      title: "Open Web URL"
+                    }, {
+                      type: "postback",
+                      title: "Call Postback",
+                      payload: "Payload for first bubble"
+                    }],
+                  }]
+                }
+              }
+            }
+        }
+        console.log('Ready to send url message');
+        request({
+            url: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: {
+                access_token: token
+            },
+            method: 'POST',
+            json: messageData
+        }, (error, response, body) => {
+            console.log('GET response from ', response);
+            if (error) {
+                console.log('Error sending message: '+ error);
+            } else if (response.body.error) {
+                var json = JSON.parse(response.request.body)
+                if (json.recipient.id === "1120359171369250")
+                    console.log(response)
+                console.log('Error: '+ response.body.error);
+            }
+            resolve();
+        })
+    }
+
+
 
     switch (operation) {
         case 'verify':
             const verifyToken = event["verify_token"]
             if (secret === verifyToken) {
-                context.succeed(parseInt(event["challenge"]))
+                context.succeed(parseInt(event["challenge"]));
             } else {
-                context.fail(new Error('Unmatch'))
+                context.fail(new Error('Unmatch'));
             }
             break
         case 'reply':
-            const messagingEvents = event.body.entry[0].messaging
+            const messagingEvents = event.body.entry[0].messaging;
             messagingEvents.forEach((messagingEvent) => {
-                const sender = messagingEvent.sender.id
+                const sender = messagingEvent.sender.id;
                 if (messagingEvent.message && messagingEvent.message.text) {
-                    const text = messagingEvent.message.text
+                    const text = messagingEvent.message.text;
                     console.log('In case reply', text);
-                    sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200), token)
+                    var p = new Promise((resolve, reject)=> {
+                        sendTextMessage(sender, text.substring(0, 200), resolve);
+                    });
+                    p.then(function(){
+                        context.succeed();
+                    });
+                } else if (messagingEvent.postback) {
+                    var event = messagingEvent;
+
+                    var senderID = event.sender.id;
+                    var recipientID = event.recipient.id;
+                    var timeOfPostback = event.timestamp;
+
+                    var payload = event.postback.payload;
+
+                    console.log("Received postback for user %d and page %d with payload '%s' at %d", senderID, recipientID, payload, timeOfPostback);
+
+                    // When a postback is called, we'll send a message back to the sender to 
+                    // let them know it was successful
+                    var p = new Promise((resolve, reject)=> {
+                        sendTextMessage(senderID, "Postback called");
+                    });
+                    p.then(function(){
+                        context.succeed();
+                    });
                 }
             })
             break
         case 'postEvent':
 
-            var marketEvent = event.body.report;
-            var generalReport = marketEvent.report;
+            var investor = event.body.id;
+            // var textArray = event.body.report;
+            var reportUrl = event.body.reportUrl;
+            var title = event.body.title;
 
-            var beginning = "Dear Client of Elite Butler,\n\nWe would like to notify you that a recent market event "+marketEvent.title+" is affecting the market to a great deal. Summarized effects are as follows:\n\n";
-            marketEvent.effects.forEach(function(effect){
-                beginning += effect+"\n"
+            //send messages one by one
+            // var i = 0;
+            // var canSend = true;
+            // while (textArray.length > 0){
+            //     if (canSend){
+            //         var pSendText = new Promise((res, rej)=>{
+            //             var text = textArray[0];
+            //             sendTextMessage(investor, text, res);
+            //             canSend = false;
+            //         });
+            //         pSendText.then(function(){
+            //             canSend = true;
+            //             textArray.shift();
+            //         });
+            //     }
+            // }
+            var message = "Dear Client,\n\nAttached please find the link for the latest market event report.\n\nIf you still find concerns, please contact "+phoneNumber;
+            var p1 = new Promise((resolve, reject)=>{
+                sendUrlMessage(investor, reportUrl, title, resolve)
             });
-            beginning += "\n"
-
-            var ending = "\n\nIf you still find concerns, please contact "+phoneNumber+".\n\nRegards,\nElite Butler"
-
-            var normalReport = beginning + "Fortunately, after reviewing your profile, you are not largely affected by this market event." + ending
-
-            var affectedInvestors = event.body.affectedInvestors;
-            investorList.forEach(function(investor){
-                var report = "";
-                if (!(investor in affectedInvestors)) {
-                    report = normalReport;
-                } else {
-                    report = beginning + "After reviewing you profile, we found that this market event may affect you to a certain extent.\n\n";
-                    if (affectedInvestors[investor].positive.length != 0){
-                        report += "The following companies you are engaged in are positively affected by this market event:\n\n";
-                        affectedInvestors[investor].positive.forEach(function(company){
-                            report += company+"\n";
-                        });
-                    } 
-                    report += "\n";
-                    if (affectedInvestors[investor].negative.length != 0){
-                        report += "The following companies you are engaged in are negatively affected by this market event:\n\n";
-                        affectedInvestors[investor].negative.forEach(function(company){
-                            report += company+"\n";
-                        });
-                    }
-                    report += "\n\nWe suggest that you take immediate response to better enhance your profile.";
-                    report += ending;
-                }
-                console.log("Report for investor #"+investor+": "+report);
+            var p2 = new Promise((resolve, reject)=>{
+                sendTextMessage(investor, message, resolve);
             });
-            context.succeed()
+            Promise.all([p1, p2]).then(()=>{
+                context.succeed();
+            });
+
+            // var marketEvent = event.body.report;
+            // var generalReport = marketEvent.report;
+
+            // var beginning = "A recent market event "+marketEvent.title+" is affecting the market to a great deal. Summarized effects:\n\n";
+            // marketEvent.effects.forEach(function(effect){
+            //     beginning += effect+"\n"
+            // });
+            // beginning += "\n"
+
+            // var ending = "\n\nIf you still find concerns, please contact "+phoneNumber
+
+            // var normalReport = "After reviewing your profile, you are not largely affected by this market event."+ending
+
+            // // prepare an array of promises, because sending messages is async and need to wait for them all to complete
+            // var promises = [];
+
+            // var affectedInvestors = event.body.affectedInvestors;
+            // investorList.forEach(function(investor){
+
+            //     var p = new Promise((resolve, reject) => {
+            //         var textArray = [];
+            //         textArray.push(beginning);
+
+            //         if (!(investor in affectedInvestors)) {
+            //             textArray.push(normalReport);
+            //         } else {
+            //             var report = "This market event may affect you to a certain extent.\n\n";
+            //             if (affectedInvestors[investor].positive.length != 0){
+            //                 report += "The following companies you are engaged in are positively affected:\n\n";
+            //                 affectedInvestors[investor].positive.forEach(function(company){
+            //                     report += company+"\n";
+            //                 });
+            //             } 
+                        
+            //             textArray.push(report);
+
+            //             report = "";
+            //             if (affectedInvestors[investor].negative.length != 0){
+            //                 report += "The following companies you are engaged in are negatively affected:\n\n";
+            //                 affectedInvestors[investor].negative.forEach(function(company){
+            //                     report += company+"\n";
+            //                 });
+            //             }
+            //             report += "\nWe suggest that you take immediate response to better enhance your profile.";
+                        
+            //             textArray.push(report);
+            //         }
+            //         textArray.push(ending);
+
+            //         // send text in pieces, one by one
+            //         // var i = 0;
+            //         // var canSend = true;
+            //         // while (textArray.length > 0){
+            //         //     if (canSend){
+            //         //         var pSendText = new Promise((res, rej)=>{
+            //         //             var text = textArray[0];
+            //         //             sendTextMessage(investor, text, res);
+            //         //             canSend = false;
+            //         //         });
+            //         //         pSendText.then(function(){
+            //         //             canSend = true;
+            //         //             textArray.shift();
+            //         //         });
+            //         //     }
+            //         // }
+            //         var report = textArray.join('\n');
+            //         sendTextMessage(investor, report, resolve);
+            //     });
+            //     promises.push(p);
+            // });
+            // Promise.all(promises).then(()=>{
+            //     context.succeed();
+            // });
+            //context.succeed()
             break
         default:
             context.fail(new Error('Unrecognized operation "' + operation + '"'))
--- functions/postEvent/handler.js
+++ functions/postEvent/handler.js
@@ -1,8 +1,12 @@
 'use strict';
 
 var request = require('request')
-// var aws = require('aws-sdk')
-// aws.config.region = 'ap-northeast-1';
+var aws = require('aws-sdk')
+aws.config.region = 'ap-northeast-1';
+
+// define hard-coded data
+var phoneNumber = "0000-0000" // phone number of Elite Butler
+var investorList = ["1", "2", "3", "4", "5", "6", "7", "1120359171369250"]; // messenger ids of registered clients
 
 // For proof of concept, the userData are hard-coded instead of retrieved from database.
 // User ids for messaging are stored at the time they register with the FB fanpage.
@@ -92,32 +96,6 @@ module.exports.handler = function(event, context) {
    //              }
 			// });
 
-            // var id = "1120359171369250"
-            // var text = "Hello"
-            // var access_token = "EAAYiiUwupN0BAFze3uVHm81075dwg4BDK2qvha65yhddrXCrRUDLmrQpiWQZB9eHAeqaMFMYT80JRgvK9mgYEKmQ8dppPmwvhk5I106kdUHmEa1Y1vyvlalDPX64f08EmqDcusZCYFxZBbecoZBxdbELkVFKthIcWdqZAuAGXVgZDZD"
-            // request({
-            //     url: 'https://graph.facebook.com/v2.6/me/messages',
-            //     qs: {
-            //         access_token: access_token
-            //     },
-            //     method: 'POST',
-            //     json: {
-            //         recipient: {
-            //             id: id
-            //         },
-            //         message: {
-            //             text: text
-            //         }
-            //     }
-            // }, (error, response, body) => {
-            //     console.log('GET response', response);
-            //     context.succeed(response);
-            //     if (error) {
-            //         context.fail('Error sending message: ', error);
-            //     } else if (response.body.error) {
-            //         context.fail('Error: ', response.body.error);
-            //     }
-            // })
 
             // look for affected users
             var affectedInvestorList = new Object();
@@ -149,27 +127,137 @@ module.exports.handler = function(event, context) {
                 });
             });
 
-            // combine json to send
-            var payload = {
-                affectedInvestors: affectedInvestorList,
-                report: json
-            }
+            // send individual messages one by one
 
-            request({
-                url: 'https://bga829qa2d.execute-api.ap-northeast-1.amazonaws.com/dev/bot',
-                method: 'POST',
-                json: payload
-            }, (error, response, body) => {
-                if (error) {
-                    console.log(error);
-                    context.fail('Error sending message: ', error);
-                } else {
-                    console.log(response);
-                    context.succeed(payload);
-                }
-            })
+            var marketEvent = json;
 
-            //context.succeed(json)
+            var beginning = "A recent market event "+marketEvent.title+" is affecting the market to a great deal. Summarized effects:\n\n";
+            marketEvent.effects.forEach(function(effect){
+                beginning += effect+"\n"
+            });
+            beginning += "\n"
+
+            // var ending = "\n\nIf you still find concerns, please contact "+phoneNumber
+
+            var normalReport = "After reviewing your profile, you are not largely affected by this market event."
+
+                // prepare an array of promises, because sending messages is async and need to wait for them all to complete
+            var promises = [];
+
+            var affectedInvestors = affectedInvestorList;
+            investorList.forEach(function(investor){
+
+                var p = new Promise((resolve, reject) => {
+                    var textArray = [];
+                    textArray.push(beginning);
+
+                    if (!(investor in affectedInvestors)) {
+                        textArray.push(normalReport);
+                    } else {
+                        var report = "This market event may affect you to a certain extent.\n\n";
+                        if (affectedInvestors[investor].positive.length != 0){
+                            report += "The following companies you are engaged in are positively affected:\n\n";
+                            affectedInvestors[investor].positive.forEach(function(company){
+                                report += company+"\n";
+                            });
+                        } 
+                        
+                        textArray.push(report);
+
+                        report = "";
+                        if (affectedInvestors[investor].negative.length != 0){
+                            report += "The following companies you are engaged in are negatively affected:\n\n";
+                            affectedInvestors[investor].negative.forEach(function(company){
+                                report += company+"\n";
+                            });
+                        }
+                        report += "\nWe suggest that you take immediate response to better enhance your profile.";
+                        
+                        textArray.push(report);
+                    }
+                    // textArray.push(ending);
+                    var textStr = textArray.join('\n');
+
+                    // upload report to s3
+                    var s3 = new aws.S3();
+                    var params = {
+                        Bucket: 'elite-butler-user-report', /* required */
+                        Key: investor, /* required */
+                        Body: textStr
+                    };
+                    console.log("Ready to send "+textStr);
+
+                    s3.putObject(params, function(err, data) {
+                        if (err) {
+                            console.log("Error uploading data: ", err);
+                        } else {
+                            console.log("Successfully uploaded data to elite-butler-user-report/"+investor);
+                            // var s3 = new aws.S3();
+                            // var params = {Bucket: 'elite-butler-user-report', Key: investor};
+                            // s3.getSignedUrl('getObject', params, function (err, url) {
+                            //     if (err) {
+                            //         console.log("Error uploading data: ", err);
+                            //     } else {
+                            //         console.log("The URL is", url);
+                            //         var payload = {
+                            //             id: investor,
+                            //             reportUrl: url
+                            //         }
+
+                            //         request({
+                            //             url: 'https://bga829qa2d.execute-api.ap-northeast-1.amazonaws.com/dev/bot',
+                            //             method: 'POST',
+                            //             json: payload
+                            //         }, (error, response, body) => {
+                            //             if (error) {
+                            //                 console.log(error);
+                            //                 // context.fail('Error sending message: ', error);
+                            //                 reject();
+                            //             } else {
+                            //                 console.log(response);
+                            //                 resolve();
+                            //             }
+                            //         })
+                            //     }
+                            // });
+                            var url = 'https://elite-butler-user-report.s3-ap-northeast-1.amazonaws.com/'+investor;
+                            console.log("The URL is", url);
+                            var payload = {
+                                id: investor,
+                                reportUrl: url,
+                                title: marketEvent.title
+                            }
+
+                            request({
+                                url: 'https://bga829qa2d.execute-api.ap-northeast-1.amazonaws.com/dev/bot',
+                                method: 'POST',
+                                json: payload
+                            }, (error, response, body) => {
+                                if (error) {
+                                    console.log(error);
+                                    // context.fail('Error sending message: ', error);
+                                    reject();
+                                } else {
+                                    console.log(response);
+                                    resolve();
+                                }
+                            })
+                        }
+                    });
+
+                    // combine json to send
+                    // var payload = {
+                    //     affectedInvestors: affectedInvestorList,
+                    //     report: json
+                    // }
+
+
+                });
+                promises.push(p);
+            });
+            Promise.all(promises).then(()=>{
+                context.succeed();
+            });
             break
         default:
             context.fail(new Error('Unrecognized operation "' + operation + '"'))
